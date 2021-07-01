7cd24d0e755a8a54f202ff89c6958e5ff75b1e27
AnimeTech5 <86360883+AnimeTech5@users.noreply.github.com>
2021-07-01 16:28:49 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,12 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
+    game.showLongText("ΦΟΡΟ{1απο2}", DialogLayout.Bottom)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
-	
+    game.showLongText("ΔΙΑΦΥΓΗ{1απο2}", DialogLayout.Bottom)
 })
 scene.setBackgroundColor(15)
 tiles.setTilemap(tilemap`level1`)
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . 
-    . . . f f f f f f . . . 
-    . f f f e e e e f f f . 
-    f f f e e e e e e f f f 
-    f f f f 4 e e e f f f f 
-    f f f 4 4 4 e e f f f f 
-    f f f 4 4 4 4 e e f f f 
-    f 4 e 4 4 4 4 4 4 e 4 f 
-    f 4 4 f f 4 4 f f 4 4 f 
-    f e 4 d d d d d d 4 e f 
-    e f e 4 d b b d d e f . 
-    e 4 d d 4 4 4 4 e f f f 
-    . e d d e 1 1 1 b f 4 e 
-    . . e e f 6 6 6 6 f . . 
-    . . . f f f f f f f . . 
-    . . . . . . . f f f . . 
-    `, SpriteKind.Player)
+let mySprite = 0
 tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
